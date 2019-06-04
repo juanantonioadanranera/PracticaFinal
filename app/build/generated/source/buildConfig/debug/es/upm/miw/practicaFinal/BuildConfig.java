2e5186f2453f133afd24a4eb810738684f649d5d
Juan Antonio Adán Ranera <jaadan@gmail.com>
2019-06-04 22:07:45 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/google-services.json

--- app/build/generated/source/buildConfig/debug/es/upm/miw/practicaFinal/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/es/upm/miw/practicaFinal/BuildConfig.java
@@ -5,7 +5,7 @@ package es.upm.miw.practicaFinal;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "es.upm.miw.firebaselogin";
+  public static final String APPLICATION_ID = "es.upm.miw.practicafinal";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
